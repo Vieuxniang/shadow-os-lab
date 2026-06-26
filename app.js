e6642999dc47e542bbe49fa9adba67549df4c5a1
Vieuxniang <Vieuxniang@users.noreply.github.com>
2026-06-26 19:07:07 +0000

    deploy: 33d25920fcacf201c3fa05a9cb9778d000fea796

---
+++ app.js
@@ -0,0 +1,5 @@
+function sendVoice() {
+    const webhook = "https://mon-serveur-n8n.local/webhook/test";
+    console.log("Envoi des données vers :", webhook);
+    alert("Données simulées envoyées à n8n !");
+}
